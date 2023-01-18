ec4c25310f44cb6a8585f00ee0bca4810ca81a29
Chris Aronis <aronis.ch@gmail.com>
2023-01-18 19:50:02 +0100

    Fixing spectral clustering for runs

--- spectralClusteringExperiments.py
+++ spectralClusteringExperiments.py
@@ -1,181 +1,83 @@
-from math import gamma
-
-import numpy as np
-import scipy.io
-from sklearn.cluster import DBSCAN, OPTICS, SpectralClustering
+import csv
 from sklearn import metrics
-from sklearn.datasets import make_blobs
-from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import SpectralClustering
+from loadDatasets import load_datasets
+import os
+import numpy as np
 
-dataset_name = "flame"
+mylist = os.listdir("/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.2/default cost function/DBSCAN")
 
-# Load datasets:
-X = scipy.io.loadmat('/home/arch/Matlab/Dimensionality Reduction/mat_files/flame.mat')
-data = X.get('X')
-labels = X.get('labels')
+for dataset in mylist:
 
+    dataset_name = dataset
+    datasets_dict = load_datasets(dataset_name)
+    numberOfLabels = max(datasets_dict["labels"]) + 1
 
-euclidean_distances = scipy.io.loadmat('/home/arch/Matlab/Dimensionality Reduction/mat_files/flame_euclidean_distances.mat')
-D = euclidean_distances.get('D')
-
-distances = scipy.io.loadmat('/home/arch/Matlab/Dimensionality Reduction/mat_files/d0_distances sin method/lambda_10000/flame_d0_distances.mat')
-d0_distances = distances.get('d0_distances')
-DMAX = distances.get('DMAX')
-DMAX_avg = distances.get('DMAX_avg')
-d_best = distances.get('d_best')
-
-Dmax_temp_value = np.amax(D)
-
-# In matlab: T=D+eye(size(D)).*Dmax_temp_value;
-T = D + np.diag(np.full(D.shape[1],1)) * Dmax_temp_value
-Dmin_temp_value= np.amin(T)
-labels = np.reshape(labels, D.shape[1])
-
-# #########################################################
-# Compute DBSCAN
-distances_interval = np.linspace(2, 10, 10, dtype=int)
-distances_interval = np.round(distances_interval)
-# Performance measures
-db_classic_homogeneity_score = []
-db_d0_homogeneity_score = []
-
-NMI_classic = []
-NMI_d0 = []
-
-RAND_index_classic = []
-RAND_index_d0 = []
-
-V_measure_classic = []
-V_measure_d0 = []
-
-
-for i in distances_interval:
-    # Classic DBscan classic:
-    db_classic = SpectralClustering(n_clusters=i, assign_labels='discretize', random_state=0).fit(data)
+    # Classic Spectral clustering classic:
+    db_classic = SpectralClustering(n_clusters=numberOfLabels, assign_labels='discretize', random_state=0).fit(datasets_dict["data"])
     db_classic_labels_pred = db_classic.labels_
-    # D0 DBscan:
 
-    d0_distances= np.exp(-0.001 * d0_distances ** 2)
-    db_d0 = SpectralClustering(n_clusters=i, assign_labels='discretize', random_state=0, affinity='precomputed').fit(d0_distances)
+    # d0 Spectral clustering:
+    d0_distances = np.exp(-datasets_dict["d0_distances"]**2 / (2* datasets_dict["d_best"]**2))
+    db_d0 = SpectralClustering(n_clusters=numberOfLabels, assign_labels='discretize', random_state=0, affinity='precomputed').fit(
+        d0_distances)
     db_d0_labels_pred = db_d0.labels_
 
-    db_classic_homogeneity_score.append(metrics.homogeneity_score(labels, db_classic_labels_pred))
-    db_d0_homogeneity_score.append(metrics.homogeneity_score(labels, db_d0_labels_pred))
+    homo_classic = metrics.homogeneity_score(datasets_dict["labels"], db_classic_labels_pred)
+    homo_d0 = metrics.homogeneity_score(datasets_dict["labels"], db_d0_labels_pred)
 
-    NMI_classic.append(metrics.adjusted_mutual_info_score(labels, db_classic_labels_pred))
-    NMI_d0.append(metrics.adjusted_mutual_info_score(labels, db_d0_labels_pred))
+    vmeasure_classic = metrics.v_measure_score(datasets_dict["labels"], db_classic_labels_pred)
+    vmeasure_d0 = metrics.v_measure_score(datasets_dict["labels"], db_d0_labels_pred)
 
-    RAND_index_classic.append(metrics.rand_score(labels, db_classic_labels_pred))
-    RAND_index_d0.append(metrics.rand_score(labels, db_d0_labels_pred))
+    rand_classic = metrics.rand_score(datasets_dict["labels"], db_classic_labels_pred)
+    rand_d0 = metrics.rand_score(datasets_dict["labels"], db_d0_labels_pred)
 
-    V_measure_classic.append(metrics.v_measure_score(labels, db_classic_labels_pred))
-    V_measure_d0.append(metrics.v_measure_score(labels, db_d0_labels_pred))
+    ami_classic = metrics.adjusted_mutual_info_score(datasets_dict["labels"], db_classic_labels_pred)
+    ami_d0 = metrics.adjusted_mutual_info_score(datasets_dict["labels"], db_d0_labels_pred)
 
-import matplotlib.pyplot as plt
+    nmi_classic = metrics.normalized_mutual_info_score(datasets_dict["labels"], db_classic_labels_pred)
+    nmi_d0 = metrics.normalized_mutual_info_score(datasets_dict["labels"], db_d0_labels_pred)
 
-plt.plot(
-        distances_interval,
-        db_classic_homogeneity_score,
-        "r*", label="classic")
+    f1_classic = metrics.f1_score(datasets_dict["labels"], db_classic_labels_pred, average='weighted')
+    f1_d0 = metrics.f1_score(datasets_dict["labels"], db_d0_labels_pred, average='weighted')
 
-plt.plot(distances_interval,
-        db_d0_homogeneity_score,
-        "b*", label="d0-method")
+    if len(set(db_classic_labels_pred)) >= 2 and len(set(db_d0_labels_pred)) >= 2:
+        silhouette_classic = metrics.silhouette_score(datasets_dict["data"], db_classic_labels_pred)
+        silhouette_d0 = metrics.silhouette_score(datasets_dict["d0_distances"], db_d0_labels_pred, metric="precomputed")
+    else:
+        silhouette_classic = 0
+        silhouette_d0 = 0
+    # Saving the results into a csv file
+    # *******************************************************
 
-plt.legend(loc="upper right")
-plt.title("{} - Homogeneity - DBscan".format(dataset_name))
-plt.xlabel("epsilon distances")
-plt.ylabel("homogeneity score")
-plt.show()
+    # open the file in the write mode
+    f = open('/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.2/default cost function/SpectralClustering/{}-SpectralClustering'
+             .format(dataset_name), 'w')
+    # create the csv writer
+    writer = csv.writer(f)
+    writer.writerow(["Number of labels", "Homogeneity classic", "Homogeneity d0", "Vmeasure classic", "Vmeasure d0", "RAND classic", "RAND d0", "AMI classic",
+                     "AMI d0", "NMI classic", "NMI d0", "F1 classic", "F1 d0", "Silhouette coefficient classic", "Silhouette coefficient d0"])
 
+    writer.writerow([numberOfLabels, homo_classic, homo_d0, vmeasure_classic, vmeasure_d0, rand_classic, rand_d0, ami_classic, ami_d0, nmi_classic, nmi_d0,
+                                 f1_classic, f1_d0, silhouette_classic, silhouette_d0])
 
-
-plt.plot(
-        distances_interval,
-        NMI_classic,
-        "r--", label="classic")
-plt.plot(
-        distances_interval,
-        NMI_d0,
-        "b--", label="d0-method")
-
-        #markeredgecolor="k",
-        #markersize=10,
-
-plt.legend(loc="upper right")
-plt.title("{} - NMI - DBscan".format(dataset_name))
-plt.xlabel("epsilon distances")
-plt.ylabel("NMI score")
-plt.show()
-
-plt.plot(
-        distances_interval,
-        RAND_index_classic,
-        "r--",label="classic")
-plt.plot(
-        distances_interval,
-        RAND_index_d0,
-        "b--", label="d0-method")
-
-        #markeredgecolor="k",
-        #markersize=10,
+    f.close()
 
 
+#Chat GPT response on how to calculate the affinity matrix
 
-
-plt.legend(loc="upper right")
-plt.title("{} - Rand - DBscan".format(dataset_name))
-plt.xlabel("epsilon distances")
-plt.ylabel("Rand score")
-plt.title("Rand")
-plt.show()
-
-plt.plot(
-        distances_interval,
-        V_measure_classic,
-        "r--", label="Classic")
-plt.plot(
-        distances_interval,
-        V_measure_d0,
-        "b--", label="d0-method")
-
-        #markeredgecolor="k",
-        #markersize=10,
-
-plt.legend(loc="upper right")
-plt.title("{} - Vmeasure - DBscan".format(dataset_name))
-plt.show()
-
-print("debugger point")
-
-# Experimenting
-# plt.plot(
-#         distances_interval,
-#         db_classic_homogeneity_score,
-#         "r*", label="classic")
+# from sklearn.cluster import SpectralClustering
+# import numpy as np
 #
+# # create a distance matrix
+# D = np.array([[0, 2, 4], [2, 0, 2], [4, 2, 0]])
 #
-# plt.legend(loc="upper right")
-# plt.title("{} - Homogeneity - DBscan".format(dataset_name))
-# plt.xlabel("epsilon distances")
-# plt.ylabel("homogeneity score")
-# plt.show()
-
-
-
-
-from sklearn.cluster import SpectralClustering
-import numpy as np
-
-# create a distance matrix
-D = np.array([[0, 2, 4], [2, 0, 2], [4, 2, 0]])
-
-# define sigma
-sigma = 1
-
-# calculate the affinity matrix
-A = np.exp(-D**2/(2*sigma**2))
-
-# perform spectral clustering
-sc = SpectralClustering(n_clusters=2)
-clusters = sc.fit_predict(A)
+# # define sigma
+# sigma = 1
+#
+# # calculate the affinity matrix
+# A = np.exp(-D**2/(2*sigma**2))
+#
+# # perform spectral clustering
+# sc = SpectralClustering(n_clusters=2)
+# clusters = sc.fit_predict(A)
