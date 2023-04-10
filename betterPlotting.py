b3bf27202542a35224164943a5d5ee56cce3f57e
Chris Aronis <aronis.ch@gmail.com>
2023-04-10 21:04:26 +0200

    improved plotting functions + clearing up + added some comments

--- betterPlotting.py
+++ betterPlotting.py
@@ -1,15 +1,44 @@
 import matplotlib.pyplot as plt
 import pickle
-import os
+from matplotlib import ticker
+
+def colorTickLabelsAndPlot(row, column, axesNew, axesOld):
+    axesNew[row][column].plot(axesOld.lines[0].get_xdata(), axesOld.lines[0].get_ydata(), "r--", label="classic")
+    axesNew[row][column].plot(axesOld.lines[1].get_xdata(), axesOld.lines[1].get_ydata(), "b--", label="d0_method")
+
+    ymax = max(axesNew[row][column].lines[0].get_ydata())
+    ymax2 = max(axesNew[row][column].lines[1].get_ydata())
+
+    if abs(ymax - ymax2) > 0.06: # so that the two ticks won't overlap
+        axesNew[row][column].yaxis.set_major_locator(ticker.FixedLocator([ymax, ymax2]))
+        flag = 0
+        for t in axesNew[row][column].yaxis.get_ticklabels():
+            if flag == 0:
+                t.set_color("red")
+                flag = 1
+            else:
+                t.set_color("blue")
+    else:
+        # there is overlap, therefore we show only the tick with the biggest value
+        if ymax > ymax2:
+            axesNew[row][column].yaxis.set_major_locator(ticker.FixedLocator([ymax]))
+            t = axesNew[row][column].yaxis.get_ticklabels()
+            t[0].set_color("red")
+        else:
+            axesNew[row][column].yaxis.set_major_locator(ticker.FixedLocator([ymax2]))
+            t = axesNew[row][column].yaxis.get_ticklabels()
+            t[0].set_color("blue")
+
+    axesNew[row][column].tick_params(axis='y', labelsize=7)
+    axesNew[row][column].yaxis.set_label_coords(-0.25, 0.50)
 
 def betterPlotting(saveToFile, pathName):
-    mylist = os.listdir("/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.3/default cost function/DBSCAN")
     row = 0
     fig, axesNew = plt.subplots(5, 5)
-    for dataset in ["breast_cancer", "coil", "digits", "genes", "isolet"]:
+    for dataset in ["breast_cancer", "swiss_roll3D", "flame", "olivetti", "spiral"]:
 
-        with open("/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.3/default cost function/"
-                  f"OPTICS/Processed datasets/new_processed_figures/{dataset}-OPTICS-metrics.pkl", 'rb') as fid:
+        with open("/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.3/default cost function/CommonNN/Processed datasets/new_Processed_Figures/"
+                  f"{dataset}-commonNN-metrics.pkl", 'rb') as fid:
             axes = pickle.load(fid)
             fid.close()
 
@@ -20,37 +49,32 @@ def betterPlotting(saveToFile, pathName):
         axes5 = axes[4].axes
 
         # Plot the data from the first subplot in the new figure
-        axesNew[row][0].plot(axes1.lines[0].get_xdata(), axes1.lines[0].get_ydata(), "r--", label="classic")
-        axesNew[row][0].plot(axes1.lines[1].get_xdata(), axes1.lines[1].get_ydata(), "b--", label="d0_method")
+        colorTickLabelsAndPlot(row, 0, axesNew, axes1)
 
         axesNew[row][0].legend(loc="upper right")
         if row == 0:
             axesNew[row][0].set_title("F1")
         axesNew[row][0].set_ylabel(dataset)
 
-        axesNew[row][1].plot(axes2.lines[0].get_xdata(), axes2.lines[0].get_ydata(), "r--", label="classic")
-        axesNew[row][1].plot(axes2.lines[1].get_xdata(), axes2.lines[1].get_ydata(), "b--", label="d0_method")
+        colorTickLabelsAndPlot(row, 1, axesNew, axes2)
 
         axesNew[row][1].legend(loc="upper right")
         if row == 0:
             axesNew[row][1].set_title("Homogeneity")
 
-        axesNew[row][2].plot(axes3.lines[0].get_xdata(), axes3.lines[0].get_ydata(), "r--", label="classic")
-        axesNew[row][2].plot(axes3.lines[1].get_xdata(), axes3.lines[1].get_ydata(), "b--", label="d0_method")
+        colorTickLabelsAndPlot(row, 2, axesNew, axes3)
 
         axesNew[row][2].legend(loc="upper right")
         if row == 0:
             axesNew[row][2].set_title("AMI")
 
-        axesNew[row][3].plot(axes4.lines[0].get_xdata(), axes4.lines[0].get_ydata(), "r--", label="classic")
-        axesNew[row][3].plot(axes4.lines[1].get_xdata(), axes4.lines[1].get_ydata(), "b--", label="d0_method")
+        colorTickLabelsAndPlot(row, 3, axesNew, axes4)
 
         axesNew[row][3].legend(loc="upper right")
         if row == 0:
            axesNew[row][3].set_title("Vmeasure")
 
-        axesNew[row][4].plot(axes5.lines[0].get_xdata(), axes5.lines[0].get_ydata(), "r--", label="classic")
-        axesNew[row][4].plot(axes5.lines[1].get_xdata(), axes5.lines[1].get_ydata(), "b--", label="d0_method")
+        colorTickLabelsAndPlot(row, 4, axesNew, axes5)
 
         axesNew[row][4].legend(loc="upper right")
         if row == 0:
@@ -66,14 +90,16 @@ def betterPlotting(saveToFile, pathName):
             fid.close()
     plt.close(fig)
 
-def main():
 
-    saveToFile = False
-    fileNameToStore = "Optics20figs"
-    pathName = "/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.3/default cost function/" \
-               "OPTICS/Processed datasets/new_processed_figures/{}" \
-        .format(fileNameToStore)
-    betterPlotting(saveToFile, pathName)
+# Creates a 5*5 subplot with results that are stored in previously created pickle files
+def main():
+    fileNameToStore = "CommonNN20figs"
+    pathName = "/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.3/default cost function/CommonNN/" \
+               f"Processed datasets/new_Processed_Figures/{fileNameToStore}".format(fileNameToStore)
+
+    saveToFile = True
+    if saveToFile:
+        betterPlotting(saveToFile, pathName)
 
     openFile = True
     if openFile:
--- plotTesting.py
+++ plotTesting.py
@@ -3,34 +3,16 @@ import pickle
 import os
 from matplotlib import ticker
 
+
+# Opening created pickle files for inspection
+
 mylist = os.listdir("/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.3/default cost function/DBSCAN")
-for dataset in mylist:
+fileNameToOpen = "Optics20figs"
 
-    with open("/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.3/default cost function/"
-              f"OPTICS/Processed datasets/new_processed_figures/{dataset}-OPTICS-metrics.pkl", 'rb') as fid:
-        axes = pickle.load(fid)
+with open("/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.3/default cost function/"\
+               f"OPTICS/Processed datasets/new_processed_figures/{fileNameToOpen}.pkl", 'rb') as fid:
+    axes = pickle.load(fid)
 
-    plt.suptitle(f'{dataset}')
-
-
-    for subplot in range(0,4):
-        ymax = max(axes[subplot].lines[0].get_ydata())
-        ymax2 = max(axes[subplot].lines[1].get_ydata())
-
-        if abs(ymax-ymax2) > 0.04:
-            continue
-
-        if ymax > ymax2:
-            axes[subplot].yaxis.set_major_locator(ticker.FixedLocator([ymax]))
-            t = axes[subplot].yaxis.get_ticklabels()
-            t[0].set_color("red")
-        else:
-            axes[subplot].yaxis.set_major_locator(ticker.FixedLocator([ymax2]))
-            t = axes[subplot].yaxis.get_ticklabels()
-            t[0].set_color("blue")
-
-    ax = plt.gca()
-    ax.figure.set_size_inches(30, 5)
     plt.show()
 
 
