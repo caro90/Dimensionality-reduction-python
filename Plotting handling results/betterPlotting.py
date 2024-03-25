314c0f8051d1cdfea6a72c02de7da0462d4463e6
Chris Aronis <aronis.ch@gmail.com>
2024-03-25 13:01:32 +0100

    improved plotting to incorporate cl vs do vs exp

--- Plotting handling results/betterPlotting.py
+++ Plotting handling results/betterPlotting.py
@@ -1,8 +1,17 @@
 import matplotlib.pyplot as plt
 import pickle
 from matplotlib import ticker
+import os
+from pathlib import Path
+from matplotlib.ticker import MaxNLocator
 
-def colorTickLabelsAndPlot(row, column, axesNew, axesOld):
+
+def format_ticks(x, pos):
+    # Define a custom tick formatter function
+    return f'{x:.2f}'
+
+
+def color_tick_labels_and_plot_euclid_piecewiselinear(row, column, axesNew, axesOld):
     axesNew[row][column].plot(axesOld.lines[0].get_xdata(), axesOld.lines[0].get_ydata(), "r--", label="classic")
     axesNew[row][column].plot(axesOld.lines[1].get_xdata(), axesOld.lines[1].get_ydata(), "b--", label="d0_method")
 
@@ -52,111 +61,262 @@ def colorTickLabelsAndPlot(row, column, axesNew, axesOld):
         '{:.2f}'.format)  # reducing the number of decimal points that are being displayed
     axesNew[row][column].set_ylim(0)
 
-def betterPlotting(saveToFile, pathName, clustering_method, ylabel):
-    row = 0
-    fig, axesNew = plt.subplots(5, 5)
-    """ 
-    OPTICS datasets pick up order:
-    ["isolet", "coil", "digits", "genes", "phoneme"]
-    ["aggregation", "breast_cancer", "D31", "diabetes", "flame"]
-    ["iris", "moons_1000", "olivetti", "pathbased", "R15"]
-    ["spiral", "swiss_roll2D", "swiss_roll3D", "Umist", "wine"]
-    """
 
-    """ 
-    CommonNN datasets pick up order:
-    ["isolet", "coil", "digits", "genes", "phoneme"]
-    ["aggregation", "breast_cancer", "D31", "diabetes", "flame"]
-    ["iris", "moons_1000", "olivetti", "pathbased", "R15"]
-    ["spiral", "swiss_roll2D", "swiss_roll3D", "Umist", "wine"]
-    """
+def color_tick_labels_and_plot_euclid_piecewiselinear_exp(row, column, axesNew, axesOld):
+    axesNew[row][column].plot(axesOld.lines[0].get_xdata(), axesOld.lines[0].get_ydata(),
+                              linestyle=axesOld.lines[0].get_linestyle(), label="Euclidean")
+    axesNew[row][column].plot(axesOld.lines[1].get_xdata(), axesOld.lines[1].get_ydata(),
+                              linestyle=axesOld.lines[1].get_linestyle(), label="Piecewise linear d0")
+    axesNew[row][column].plot(axesOld.lines[2].get_xdata(), axesOld.lines[2].get_ydata(),
+                              linestyle=axesOld.lines[2].get_linestyle(), label="Exponential d0")
 
-    for dataset in ["isolet", "coil", "digits", "genes", "phoneme"]:
+    axesNew[row][column].yaxis.set_major_formatter(axesOld.axes.yaxis.get_major_formatter())
+    axesNew[row][column].yaxis.set_major_locator(axesOld.axes.yaxis.get_major_locator())
 
-        with open(f"/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.3/default cost function/{clustering_method}/Processed datasets/new_processed_figures/"
-                  f"{dataset}-{clustering_method}-metrics.pkl", 'rb') as fid:
-            axes = pickle.load(fid)
+    # Set custom tick formatter for y-axis
+    axesNew[row][column].yaxis.set_major_formatter(ticker.FuncFormatter(format_ticks))
+
+    # Transfer tick colors from previous axes
+    for tick_label in axesOld.axes.yaxis.get_ticklabels():
+        axesNew[row][column].yaxis.get_major_ticks()[0].label1.set_color(tick_label.get_color())
+
+    # Transfer line colors
+    for source_line, target_line in zip(axesOld.axes.lines, axesNew[row][column].lines):
+        target_line.set_color(source_line.get_color())
+
+    if row == 0 and column == 4:
+        # Position of the legend
+        axesNew[row][column].legend(loc='upper right', bbox_to_anchor=(1.60, 1.05))
+
+
+def density_based_clustering_20figures_plotting(list_of_datasets, saveToFile, load_pickles_loc_path_folder, clustering_method, ylabel, filename):
+
+    counter = 0
+    for datasets in list_of_datasets:
+        row = 0
+        fig, axesNew = plt.subplots(5, 5)
+        for dataset in datasets:
+            counter += 1
+            path_to_load_pickle = (f"{load_pickles_loc_path_folder}/{clustering_method}/{dataset}/Best/"
+                                   f"{dataset}-{clustering_method}-metrics.pkl")
+            if clustering_method == "OPTICS":
+                path_to_load_pickle = (f"{load_pickles_loc_path_folder}/{clustering_method}/{dataset}/"
+                                       f"{dataset}-{clustering_method}-metrics.pkl")
+            with open(path_to_load_pickle, 'rb') as fid:
+                loaded_axes = pickle.load(fid)
+                fid.close()
+
+            # Plot the data from the first subplot in the new figure
+            color_tick_labels_and_plot_euclid_piecewiselinear_exp(row, 0, axesNew, loaded_axes[0])
+            if row == 0:
+                axesNew[row][0].set_title("F1", fontsize=15)
+            axesNew[row][0].set_ylabel(dataset, fontsize=15)
+
+            color_tick_labels_and_plot_euclid_piecewiselinear_exp(row, 1, axesNew, loaded_axes[1])
+            # Use to avoid overlapping values on the x-axis
+            if dataset == 'coil':
+                axesNew[row][0].xaxis.set_major_locator(MaxNLocator(nbins=5))
+                axesNew[row][1].xaxis.set_major_locator(MaxNLocator(nbins=5))
+                axesNew[row][2].xaxis.set_major_locator(MaxNLocator(nbins=5))
+                axesNew[row][3].xaxis.set_major_locator(MaxNLocator(nbins=5))
+                axesNew[row][4].xaxis.set_major_locator(MaxNLocator(nbins=5))
+
+            if row == 0:
+                axesNew[row][1].set_title("Homogeneity", fontsize=15)
+            color_tick_labels_and_plot_euclid_piecewiselinear_exp(row, 2, axesNew, loaded_axes[2])
+
+            if row == 0:
+                axesNew[row][2].set_title("AMI", fontsize=15)
+            color_tick_labels_and_plot_euclid_piecewiselinear_exp(row, 3, axesNew, loaded_axes[3])
+
+            if row == 0:
+               axesNew[row][3].set_title("Vmeasure", fontsize=15)
+            color_tick_labels_and_plot_euclid_piecewiselinear_exp(row, 4, axesNew, loaded_axes[4])
+
+            if row == 0:
+                axesNew[row][4].set_title("RAND", fontsize=15)
+            row += 1
+
+            if row == 4:
+                axesNew[row][2].set_xlabel(f"{ylabel}", fontsize=15)
+
+        if saveToFile:
+            # Storing as a pickle file
+            # Creating a figure that can be later changed
+            with open(f"{load_pickles_loc_path_folder}/{filename}-{counter}.pkl", 'wb') as fid:
+                pickle.dump(axesNew, fid)
+                fid.close()
+
+
+def density_based_clustering_5figures_plotting(datasets, path, method_name):
+    # Creating a plot with 5 figures (Homo,Vmeasure,Rand,AMI,F1) for each dataset
+
+    for dataset in datasets:
+        dataset_name = dataset
+
+        p = Path(f"{path}/{method_name}/{dataset_name}/Best/")
+        if method_name == "OPTICS":
+            p = Path(f"{path}/{method_name}/{dataset_name}/")
+
+        # If the intermediate folders do not exist, then they are being created
+        os.makedirs(p, exist_ok=True)
+
+        matching_files_f1 = [filename for filename in p.glob('**/*.pkl') if 'f1' in str(filename)]
+        with open(f'{p}/{matching_files_f1[0].name}', 'rb') as fid:
+            fig_f1 = pickle.load(fid)
             fid.close()
 
-        axes1 = axes[0].axes
-        axes2 = axes[1].axes
-        axes3 = axes[2].axes
-        axes4 = axes[3].axes
-        axes5 = axes[4].axes
+        matching_files_homo_ami = [filename for filename in p.glob('**/*.pkl') if 'Homogeneity' in str(filename)]
+        with open(f'{p}/{matching_files_homo_ami[0].name}', 'rb') as fid:
+            fig_homo_ami = pickle.load(fid)
+            fid.close()
 
+        matching_files_vmeasure_rand = [filename for filename in p.glob('**/*.pkl') if 'Vmeasure' in str(filename)]
+        with open(f'{p}/{matching_files_vmeasure_rand[0].name}', 'rb') as fid:
+            fig_vmeasure_rand = pickle.load(fid)
+            fid.close()
+
+        fileToStore = f"{dataset_name}-{method_name}-metrics"
+
+        # Create a new figure with two identical subplots
+        fig2, ax = plt.subplots(1, 5)
+        plt.subplots_adjust(wspace=0.30)
+        # fig2.tight_layout(h_pad=5)
+
+        plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.90)
         # Plot the data from the first subplot in the new figure
-        colorTickLabelsAndPlot(row, 0, axesNew, axes1)
+        ax[0].plot(fig_f1.axes.lines[0].get_xdata(), fig_f1.axes.lines[0].get_ydata(), "r--", label="cl")
+        ax[0].plot(fig_f1.axes.lines[1].get_xdata(), fig_f1.axes.lines[1].get_ydata(), "b--", label="d0")
+        ax[0].plot(fig_f1.axes.lines[2].get_xdata(), fig_f1.axes.lines[2].get_ydata(), "g--", label="exp-d0")
 
-        if row == 0:
-            axesNew[row][0].set_title("F1", fontsize= 15)
-        axesNew[row][0].set_ylabel(dataset, fontsize=15)
-        axesNew[row][0].lines[0].set_linestyle("solid")
-        axesNew[row][0].legend(loc="upper right")
+        # Transfer ticks
+        ax[0].yaxis.set_major_formatter(fig_f1.axes.yaxis.get_major_formatter())
+        ax[0].yaxis.set_major_locator(fig_f1.axes.yaxis.get_major_locator())
+        # Transfer tick colors
+        for tick_label in fig_f1.axes.yaxis.get_ticklabels():
+            ax[0].yaxis.get_major_ticks()[0].label1.set_color(tick_label.get_color())
+        ax[0].set_title("F1")
+        ax[0].set_ylabel("Score")
 
-        colorTickLabelsAndPlot(row, 1, axesNew, axes2)
+        ax[1].plot(fig_homo_ami[0].axes.lines[0].get_xdata(), fig_homo_ami[0].axes.lines[0].get_ydata(), "r--", label="cl")
+        ax[1].plot(fig_homo_ami[0].axes.lines[1].get_xdata(), fig_homo_ami[0].axes.lines[1].get_ydata(), "b--", label="d0")
+        ax[1].plot(fig_homo_ami[0].axes.lines[2].get_xdata(), fig_homo_ami[0].axes.lines[2].get_ydata(), "g--", label="exp-d0")
 
-        # Use to avoid overlapping values on the x-axis
-        # if dataset == 'coil':
-        #     axesNew[row][0].xaxis.set_major_locator(MaxNLocator(nbins=5))
-        #     axesNew[row][1].xaxis.set_major_locator(MaxNLocator(nbins=5))
-        #     axesNew[row][2].xaxis.set_major_locator(MaxNLocator(nbins=5))
-        #     axesNew[row][3].xaxis.set_major_locator(MaxNLocator(nbins=5))
-        #     axesNew[row][4].xaxis.set_major_locator(MaxNLocator(nbins=5))
-        #-------
-        axesNew[row][1].legend(loc="upper right")
-        if row == 0:
-            axesNew[row][1].set_title("Homogeneity", fontsize= 15)
-        axesNew[row][1].lines[0].set_linestyle("solid")
-        colorTickLabelsAndPlot(row, 2, axesNew, axes3)
-        axesNew[row][2].legend(loc="upper right")
+        # Transfer ticks
+        ax[1].yaxis.set_major_formatter(fig_homo_ami[0].axes.yaxis.get_major_formatter())
+        ax[1].yaxis.set_major_locator(fig_homo_ami[0].axes.yaxis.get_major_locator())
+        # Transfer tick colors
+        for tick_label in fig_homo_ami[0].axes.yaxis.get_ticklabels():
+            ax[1].yaxis.get_major_ticks()[0].label1.set_color(tick_label.get_color())
+        ax[1].set_title("Homogeneity")
 
+        ax[2].plot(fig_homo_ami[1].axes.lines[0].get_xdata(), fig_homo_ami[1].axes.lines[0].get_ydata(), "r--", label="cl")
+        ax[2].plot(fig_homo_ami[1].axes.lines[1].get_xdata(), fig_homo_ami[1].axes.lines[1].get_ydata(), "b--", label="d0")
+        ax[2].plot(fig_homo_ami[1].axes.lines[2].get_xdata(), fig_homo_ami[1].axes.lines[2].get_ydata(), "g--", label="exp-d0")
 
-        if row == 0:
-            axesNew[row][2].set_title("AMI", fontsize= 15)
-        axesNew[row][2].lines[0].set_linestyle("solid")
+        # Transfer ticks
+        ax[2].yaxis.set_major_formatter(fig_homo_ami[1].axes.yaxis.get_major_formatter())
+        ax[2].yaxis.set_major_locator(fig_homo_ami[1].axes.yaxis.get_major_locator())
+        # Transfer tick colors
+        for tick_label in fig_homo_ami[1].axes.yaxis.get_ticklabels():
+            ax[2].yaxis.get_major_ticks()[0].label1.set_color(tick_label.get_color())
+        ax[2].set_title("AMI")
+        ax[2].set_xlabel("epsilon distances")
 
-        colorTickLabelsAndPlot(row, 3, axesNew, axes4)
-        axesNew[row][3].legend(loc="upper right")
-        axesNew[row][1].legend(loc="upper right")
+        ax[3].plot(fig_vmeasure_rand[0].axes.lines[0].get_xdata(), fig_vmeasure_rand[0].axes.lines[0].get_ydata(), "r--", label="cl")
+        ax[3].plot(fig_vmeasure_rand[0].axes.lines[1].get_xdata(), fig_vmeasure_rand[0].axes.lines[1].get_ydata(), "b--", label="d0")
+        ax[3].plot(fig_vmeasure_rand[0].axes.lines[2].get_xdata(), fig_vmeasure_rand[0].axes.lines[2].get_ydata(), "g--", label="exp-d0")
 
-        if row == 0:
-           axesNew[row][3].set_title("Vmeasure", fontsize= 15)
-        axesNew[row][3].lines[0].set_linestyle("solid")
+        # Transfer ticks
+        ax[3].yaxis.set_major_formatter(fig_vmeasure_rand[0].axes.yaxis.get_major_formatter())
+        ax[3].yaxis.set_major_locator(fig_vmeasure_rand[0].axes.yaxis.get_major_locator())
+        # Transfer tick colors
+        for tick_label in fig_vmeasure_rand[0].axes.yaxis.get_ticklabels():
+            ax[3].yaxis.get_major_ticks()[0].label1.set_color(tick_label.get_color())
+        ax[3].set_title("Vmeasure")
 
-        colorTickLabelsAndPlot(row, 4, axesNew, axes5)
+        ax[4].plot(fig_vmeasure_rand[1].axes.lines[0].get_xdata(), fig_vmeasure_rand[1].axes.lines[0].get_ydata(),
+                   linestyle=fig_vmeasure_rand[1].axes.lines[0].get_linestyle(),
+                   label=fig_vmeasure_rand[1].axes.lines[0].get_label(),
+                   color=fig_vmeasure_rand[1].axes.lines[0].get_color())
+        ax[4].plot(fig_vmeasure_rand[1].axes.lines[1].get_xdata(), fig_vmeasure_rand[1].axes.lines[1].get_ydata(),
+                   linestyle=fig_vmeasure_rand[1].axes.lines[1].get_linestyle(),
+                   label=fig_vmeasure_rand[1].axes.lines[1].get_label(),
+                   color=fig_vmeasure_rand[1].axes.lines[1].get_color())
+        ax[4].plot(fig_vmeasure_rand[1].axes.lines[2].get_xdata(), fig_vmeasure_rand[1].axes.lines[2].get_ydata(),
+                   linestyle=fig_vmeasure_rand[1].axes.lines[2].get_linestyle(),
+                   label=fig_vmeasure_rand[1].axes.lines[2].get_label(),
+                   color=fig_vmeasure_rand[1].axes.lines[2].get_color())
 
-        if row == 0:
-            axesNew[row][4].set_title("RAND", fontsize= 15)
-        axesNew[row][4].lines[0].set_linestyle("solid")
-        axesNew[row][4].legend(loc="upper right")
-        row += 1
+        # Transfer ticks
+        ax[4].yaxis.set_major_formatter(fig_vmeasure_rand[1].axes.yaxis.get_major_formatter())
+        ax[4].yaxis.set_major_locator(fig_vmeasure_rand[1].axes.yaxis.get_major_locator())
+        # Transfer tick colors
+        for tick_label in fig_vmeasure_rand[1].axes.yaxis.get_ticklabels():
+            ax[4].yaxis.get_major_ticks()[0].label1.set_color(tick_label.get_color())
 
-        if row == 4:
-            axesNew[row][2].set_xlabel(f"{ylabel}", fontsize= 15)
-    if saveToFile:
+        # Position of the legend
+        ax[4].legend(loc='upper left', bbox_to_anchor=(1.05, 1))
+        ax[4].set_title("Vmeasure")
+
         # Storing as a pickle file
+        path_name = f"{p}/{fileToStore}"
         # Creating a figure that can be later changed
-        with open(pathName + '.pkl', 'wb') as fid:
-            pickle.dump(axesNew, fid)
-            fid.close()
-    plt.close(fig)
+        with open(path_name + '.pkl', 'wb') as fid:
+            pickle.dump(ax, fid)
 
-# Creates a 5*5 subplot with results that are stored in previously created pickle files
+        plt.close('all')
+
+
 def main():
 
-    clustering_method = "OPTICS"
-    ylabel = "Epsilon distances"
-    fileNameToStore = f"{clustering_method}20figs-result1-new"
-    pathName = f"/home/arch/PycharmProjects/Dimensionality reduction results/Version 0.3/default cost function/{clustering_method}/" \
-               f"Processed datasets/new_processed_figures/{fileNameToStore}"
+    # Configuration settings:
 
-    saveToFile = True
-    if saveToFile:
-        betterPlotting(saveToFile, pathName, clustering_method, ylabel)
+    # Method name can be:
+    # DBSCAN or CommonNN or OPTICS
+    clustering_method = "OPTICS"
+    version = "Version 0.5"
+    sampling_function = "d0_distances_sin_method"
+    cost_function = "default cost+cost 4"
+
+    datasets = ["aggregation", "breast_cancer", "coil", "D31", "diabetes",
+                "digits", "flame", "genes", "iris", "isolet",
+                "moons_1000", "olivetti", "pathbased", "phoneme", "R15",
+                "spiral", "swiss_roll2D", "swiss_roll3D", "Umist", "wine"]
+
+    load_pickles_loc_path_folder = (f"/home/arch/PycharmProjects/Dimensionality reduction results/{version}/"
+                                    f"{sampling_function}/{cost_function}/20 percent dijkstra points - 100 percent backtracking points")
 
     openFile = True
+    saveToFile = True
+
+    # Creates a 1*5 subplot with results that are stored in previously created pickle files:
+    density_based_clustering_5figures_plotting(datasets, load_pickles_loc_path_folder, clustering_method)
+
+    # Creating a 5*5 subplot:
+    ylabel = "MinPts"
+    fileNameToStore = f"{clustering_method}20figs-result"
+
+    datasets = [["isolet", "coil", "digits", "genes", "phoneme"],
+                ["aggregation", "breast_cancer", "D31", "diabetes", "flame"],
+                ["iris", "moons_1000", "olivetti", "pathbased", "R15"],
+                ["spiral", "swiss_roll2D", "swiss_roll3D", "Umist", "wine"]]
+
+
+    if saveToFile:
+        density_based_clustering_20figures_plotting(datasets, saveToFile, load_pickles_loc_path_folder,
+                                                    clustering_method, ylabel, fileNameToStore)
+
+    # For debugging purposes
     if openFile:
-        with open(pathName+".pkl", 'rb') as fid:
+        plt.close('all')
+        with open(f"{load_pickles_loc_path_folder}/{fileNameToStore}-5.pkl", 'rb') as fid:
+            axes = pickle.load(fid)
+        with open(f"{load_pickles_loc_path_folder}/{fileNameToStore}-10.pkl", 'rb') as fid:
+            axes = pickle.load(fid)
+        with open(f"{load_pickles_loc_path_folder}/{fileNameToStore}-15.pkl", 'rb') as fid:
+            axes = pickle.load(fid)
+        with open(f"{load_pickles_loc_path_folder}/{fileNameToStore}-20.pkl", 'rb') as fid:
             axes = pickle.load(fid)
         plt.show()
 
